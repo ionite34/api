d8e08883297ae1bd0a43c72f713a8f6801324d94
ionite34 <dev@ionite.io>
2022-08-05 14:56:37 -0400

    Updated caching for detector
    Signed-off-by: ionite34 <dev@ionite.io>

--- src/api/server.py
+++ src/api/server.py
@@ -1,12 +1,14 @@
+from __future__ import annotations
+
 import logging
-import urllib.parse
+from functools import cached_property
 
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from rolldet import Detector
 
 from api import __version__
-from api.models import MessageResponse, RollDetResponse
+from api.models import MessageResponse, RollDetResponse, VersionResponse
 
 logger = logging.getLogger(__name__)
 
@@ -21,31 +23,44 @@ app.add_middleware(
 )
 
 
+class RunnerStore:
+    """Stores persistent runners."""
+
+    @cached_property
+    def rolldet(self) -> Detector:
+        """Lazy loaded rolldet.Detector"""
+        return Detector()
+
+
+runner_store = RunnerStore()
+
+
 @app.get("/")
-async def root():
-    return {
-        "message": "You've reached the root of the Ionite API",
-        "version": __version__,
-    }
+async def root() -> VersionResponse:
+    return VersionResponse(
+        message="You've reached the root of the Ionite API",
+        version=__version__,
+    )
 
 
 @app.get("/hello/{name}", response_model=MessageResponse)
-async def hello(name: str):
+async def hello(name: str) -> MessageResponse:
     logger.info(f"GET /hello/{name}")
     return MessageResponse(message=f"Hello {name}")
 
 
 @app.get("/rolldet/{url:path}", response_model=RollDetResponse)
-async def rolldet(url: str, request: Request):
+async def rolldet(url: str, request: Request) -> RollDetResponse:
     logger.info(f"GET /rolldet/{url}")
     x_url = request.path_params.get("url")
     x_query = request.query_params
     full_url = f"{x_url}?{x_query}"
-    logger.warning(f"url: {full_url}")
-    # Add https if not present
-    if not full_url.startswith("https://") and not full_url.startswith("http://"):
+
+    # noinspection HttpUrlsUsage
+    if not full_url.startswith(("https://", "http://")):
         full_url = f"https://{full_url}"
-    logging.info(f"Parsed URL: {full_url}")
-    detector = Detector()
+
+    logger.info(f"Parsed /rolldet/ url: {full_url}")
+    detector = runner_store.rolldet
     result = await detector.find(full_url)
     return RollDetResponse.from_dict(result.as_dict())
